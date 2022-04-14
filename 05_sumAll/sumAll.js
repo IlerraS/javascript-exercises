472791ac15b89f0a1937bd91b61f7e73a8374325
Illerra Suerte <llerra.suerte@gmail.com>
2022-04-13 19:43:11 -0500

    These are unsure commits. I hope im doing the right things -IS

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    var max = num2;
+    var min = num1;
+    if(!Number.isInteger(num2))
+    {
+        return 'ERROR';
+    }
+    else if(num1 < 0)
+    {
+        return 'ERROR';
+    }
+    if(num1 > num2)
+    {
+        max = num1;
+        min = num2;
+    }
 
+    var total = 0;
+    for(var x = min; x <= max; ++x)
+    {
+        total += x;
+    }
+    return total;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(imp2met)
+{
+  var newTemp = (imp2met - 32) * .5556;
+  return Math.round(newTemp * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(met2imp)
+{
+  var newTemp = (met2imp * (9/5)) + 32;
+  return Math.round(newTemp * 10) / 10;
 };
 
 // Do not edit below this line
